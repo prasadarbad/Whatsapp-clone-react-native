c988fa48b5b27ed8680853e93b6d4d28a7fb24c8
Prasad Arbad <prasadarbad9@gmail.com>
2023-02-19 20:00:41 +0530

    changes in navigator

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,73 +1,91 @@
 import React from "react";
-
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from "expo-font";
-import ChatListScreen from "../Screens/ChatListScreen";
-import ChatSettingScreen from "../Screens/ChatSettingScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import SettingsScreen from "../Screens/SettingsScreen";
-import ChatScreen from "../Screens/ChatScreen";
-import NewChatScreen from "../Screens/NewChatScreen";
+import { Ionicons } from "@expo/vector-icons";
+
+import ChatSettingsScreen from "../screens/ChatSettingsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import ChatListScreen from "../screens/ChatListScreen";
+import ChatScreen from "../screens/ChatScreen";
+import NewChatScreen from "../screens/NewChatScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useSelector } from "react-redux";
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{ headerTitle: '',headerShadowVisible:false }}>
-      <Tab.Screen name="ChatList" component={ChatListScreen} options={{
-        tabBarLabel: 'Chats',
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="chatbubble-outline" size={ size } color={ color }  />
-        )
-      }} />
-      <Tab.Screen name="Settings" component={SettingsScreen} options={{
-        tabBarLabel: 'Settings',
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="settings-outline" size={ size } color={ color } />
-        )
-      }}/>
+    <Tab.Navigator screenOptions={{
+      headerTitle: "",
+      headerShadowVisible: false  
+    }}>
+      <Tab.Screen
+        name="ChatList"
+        component={ChatListScreen}
+        options={{
+          tabBarLabel: "Chats",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+};
+const StackNavigator=()=>{
+  return(
+    <Stack.Navigator>
+    <Stack.Group>
+      <Stack.Screen
+        name="Home"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ChatScreen"
+        component={ChatScreen}
+        options={{
+          headerTitle: "",
+          headerBackTitle: "Back",
+        }}
+      />
+      <Stack.Screen
+        name="ChatSettings"
+        component={ChatSettingsScreen}
+        options={{
+          headerTitle: "Settings",
+          headerBackTitle: "Back",
+        }}
+      />
+    </Stack.Group>
+
+    <Stack.Group screenOptions={{ presentation: 'containedModal' }}>
+      <Stack.Screen
+        name="NewChat"
+        component={NewChatScreen}
+      />
+    </Stack.Group>
+  </Stack.Navigator>
   )
 }
 
-const MainNavigator =props=>{
+const MainNavigator = (props) => {
 
-  
- 
-return(
-    <Stack.Navigator>
-      <Stack.Group>
-    <Stack.Screen
-      name="Home"
-      component={TabNavigator}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="ChatScreen"
-      component={ChatScreen}
-      options={{
-        headerTitle:"",
-        headerBackTitle:"Back"
-      }}
-    />
-     <Stack.Screen
-      name="ChatSettings"
-      component={ChatSettingScreen}
-      options={{
-        headerTitle: "Settings",
-      }}
-    />
-    </Stack.Group>
-    <Stack.Group screenOptions={{presentation:'containedModal'}}>
-    <Stack.Screen
-      name="NewChat"
-      component={NewChatScreen}
-      
-    />
-    </Stack.Group>
-  </Stack.Navigator>
-);
+  const userData = useSelector(state=>state.auth.userData);
+  const storedUsers=useSelector(state=>state.users.storedUsers);
+  return (
+   <StackNavigator/>
+  );
 };
+
 export default MainNavigator;
